3d25a28a46edc80afdc942d41550ab455c19c5bf
Sabrina Dubroca <sd@queasysnail.net>
2014-09-10 23:23:02 +0200

    ipv6: clean up anycast when an interface is destroyed
    If we try to rmmod the driver for an interface while sockets with
    setsockopt(JOIN_ANYCAST) are alive, some refcounts aren't cleaned up
    and we get stuck on:
    
      unregister_netdevice: waiting for ens3 to become free. Usage count = 1
    
    If we LEAVE_ANYCAST/close everything before rmmod'ing, there is no
    problem.
    
    We need to perform a cleanup similar to the one for multicast in
    addrconf_ifdown(how == 1).
    
    BUG: 18902601
    Change-Id: I6d51aed5755eb5738fcba91950e7773a1c985d2e
    Signed-off-by: Sabrina Dubroca <sd@queasysnail.net>
    Acked-by: Hannes Frederic Sowa <hannes@stressinduktion.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Conflicts:
    	include/net/addrconf.h

--- include/net/addrconf.h
+++ include/net/addrconf.h
@@ -166,10 +166,10 @@ extern void ipv6_sock_ac_close(struct sock *sk);
 
 extern int ipv6_dev_ac_inc(struct net_device *dev, const struct in6_addr *addr);
 extern int __ipv6_dev_ac_dec(struct inet6_dev *idev, const struct in6_addr *addr);
-extern int ipv6_chk_acast_addr(struct net *net, struct net_device *dev,
-			       const struct in6_addr *addr);
-
 u32 addrconf_rt_table(const struct net_device *dev, u32 default_table);
+extern bool ipv6_chk_acast_addr(struct net *net, struct net_device *dev,
+				const struct in6_addr *addr);
+extern void ipv6_ac_destroy_dev(struct inet6_dev *idev);
 
 
 /* Device notifier */
